9b5371b2d76026507bbf13a3eac4f0573167d248
ppfei03 <p_pfei03@uni-muenster.de>
2024-12-31 13:54:52 +0100

    renew

--- DBS_Webserver/server.js
+++ DBS_Webserver/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb');
 const path = require('path');
 const { exec } = require('child_process');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
@@ -9,17 +10,12 @@ const PORT = 3000;
 // DynamoDB Client konfigurieren
 const dynamoDbClient = new DynamoDBClient({ region: 'us-east-1' });
 
-
-const cors = require('cors');
-
+// CORS konfigurieren
 app.use(cors({
-    origin: 'https://dbs.philipppfeiffer.de', // Die erlaubte Origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Erlaubte HTTP-Methoden
-    credentials: true // Falls Cookies oder Authentifizierung erforderlich sind
+    origin: 'https://dbs.philipppfeiffer.de',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
 }));
-
-
-app.use(cors());
 
 // Statische Dateien bereitstellen
 app.use(express.static('public'));
@@ -36,7 +32,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Dynamisches Routing für andere HTML-Dateien
+// Dynamisches Routing für HTML-Dateien
 app.get('/:file', (req, res) => {
     const filePath = path.join(__dirname, 'public', req.params.file);
     res.sendFile(filePath, (err) => {
@@ -45,197 +41,63 @@ app.get('/:file', (req, res) => {
             res.status(404).send('Seite nicht gefunden');
         }
     });
-});;
-
-// API-Route, um Daten aus DynamoDB zu holen
-app.get('/api/data/BoxesTable', async (req, res) => {
-    const params = {
-        TableName: 'BoxesTable', // Ersetzen Sie dies durch den Namen Ihrer DynamoDB-Tabelle
-    };
-
-    try {
-        const command = new ScanCommand(params);
-        const data = await dynamoDbClient.send(command);
-        res.json(data.Items);
-    } catch (err) {
-        console.error('Fehler beim Abrufen der Daten:', err);
-        res.status(500).send('Fehler beim Abrufen der Daten');
-    }
 });
 
-// API-Route, um Daten aus DynamoDB zu holen
-app.get('/api/data/SensorsTable', async (req, res) => {
-    const params = {
-        TableName: 'SensorsTable', // Ersetzen Sie dies durch den Namen Ihrer DynamoDB-Tabelle
-    };
-
-    try {
-        const command = new ScanCommand(params);
-        const data = await dynamoDbClient.send(command);
-        res.json(data.Items);
-    } catch (err) {
-        console.error('Fehler beim Abrufen der Daten:', err);
-        res.status(500).send('Fehler beim Abrufen der Daten');
-    }
-});
-
-// API-Route, um Daten aus DynamoDB zu holen
-app.get('/api/data/SensorsTableLasy', async (req, res) => {
-    const { lastKey } = req.query;
-
-    const params = {
-        TableName: 'SensorsTable',
-        Limit: 50, // Anzahl der Datensätze pro Anfrage
-    };
-
-    if (lastKey) {
-        params.ExclusiveStartKey = JSON.parse(decodeURIComponent(lastKey));
-    }
-
-    try {
-        const command = new ScanCommand(params);
-        const data = await dynamoDbClient.send(command);
-
-        res.json({
-            items: data.Items,
-            lastKey: data.LastEvaluatedKey ? encodeURIComponent(JSON.stringify(data.LastEvaluatedKey)) : null,
-        });
-    } catch (err) {
-        console.error('Fehler beim Abrufen der Daten:', err);
-        res.status(500).send('Fehler beim Abrufen der Daten');
-    }
-});
-
-// API-Route, um Daten aus DynamoDB zu holen
-app.get('/api/data/BoxesTableLasy', async (req, res) => {
-    const { lastKey } = req.query;
-
-    const params = {
-        TableName: 'BoxesTable',
-        Limit: 50, // Anzahl der Datensätze pro Anfrage
-    };
-
-    if (lastKey) {
-        params.ExclusiveStartKey = JSON.parse(decodeURIComponent(lastKey));
-    }
-
-    try {
-        const command = new ScanCommand(params);
-        const data = await dynamoDbClient.send(command);
-
-        res.json({
-            items: data.Items,
-            lastKey: data.LastEvaluatedKey ? encodeURIComponent(JSON.stringify(data.LastEvaluatedKey)) : null,
-        });
-    } catch (err) {
-        console.error('Fehler beim Abrufen der Daten:', err);
-        res.status(500).send('Fehler beim Abrufen der Daten');
-    }
-});
-
-// API-Route, um Daten aus DynamoDB zu holen
-app.get('/api/data/MeasurementsTableLasy', async (req, res) => {
-    const { lastKey } = req.query;
-
-    const params = {
-        TableName: 'MeasurementsTable',
-        Limit: 50, // Anzahl der Datensätze pro Anfrage
-    };
-
-    if (lastKey) {
-        params.ExclusiveStartKey = JSON.parse(decodeURIComponent(lastKey));
-    }
-
-    try {
-        const command = new ScanCommand(params);
-        const data = await dynamoDbClient.send(command);
-
-        res.json({
-            items: data.Items,
-            lastKey: data.LastEvaluatedKey ? encodeURIComponent(JSON.stringify(data.LastEvaluatedKey)) : null,
-        });
-    } catch (err) {
-        console.error('Fehler beim Abrufen der Daten:', err);
-        res.status(500).send('Fehler beim Abrufen der Daten');
-    }
-});
-
-// API-Route zum Ausführen des Python-Skripts
-app.get('/run-python/insertData', (req, res) => {
-    // Relativer Pfad zum Python-Skript
-    const scriptPath = path.join(__dirname, 'scripts', 'importDataToDynamoDB.py');
-
+// Funktion zum Ausführen von Python-Skripten
+const runPythonScript = (scriptName, res) => {
+    const scriptPath = path.join(__dirname, 'scripts', scriptName);
     exec(`python3 ${scriptPath}`, (error, stdout, stderr) => {
         if (error) {
             console.error(`Fehler beim Ausführen des Python-Skripts: ${error.message}`);
-            return res.status(500).send('Fehler beim Ausführen des Skripts.');
-        }
-        if (stderr) {
+            res.status(500).send(`Fehler beim Ausführen des Skripts: ${error.message}`);
+        } else if (stderr) {
             console.error(`Fehler im Python-Skript: ${stderr}`);
-            return res.status(500).send(`Fehler im Skript: ${stderr}`);
-        }
-        console.log(`Ergebnis des Python-Skripts: ${stdout}`);
-        res.send(`Skript erfolgreich ausgeführt: ${stdout}`);
-    });
-});
-
-// API-Route zum Ausführen des Python-Skripts
-app.get('/run-python/insertDataMeasure', (req, res) => {
-    // Relativer Pfad zum Python-Skript
-    const scriptPath = path.join(__dirname, 'scripts', 'importMeasureToDynamoDB.py');
-
-    exec(`python3 ${scriptPath}`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Fehler beim Ausführen des Python-Skripts: ${error.message}`);
-            return res.status(500).send('Fehler beim Ausführen des Skripts.');
-        }
-        if (stderr) {
-            console.error(`Fehler im Python-Skript: ${stderr}`);
-            return res.status(500).send(`Fehler im Skript: ${stderr}`);
-        }
-        console.log(`Ergebnis des Python-Skripts: ${stdout}`);
-        res.send(`Skript erfolgreich ausgeführt: ${stdout}`);
-    });
-});
-
-    // API-Route zum Ausführen des Python-Skripts
-app.get('/run-python/deleteData', (req, res) => {
-    // Relativer Pfad zum Python-Skript
-    const scriptPath = path.join(__dirname, 'scripts', 'delteTablesAndData.py');
-    
-        exec(`python3 ${scriptPath}`, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Fehler beim Ausführen des Python-Skripts: ${error.message}`);
-                return res.status(500).send('Fehler beim Ausführen des Skripts.');
-            }
-            if (stderr) {
-                console.error(`Fehler im Python-Skript: ${stderr}`);
-                return res.status(500).send(`Fehler im Skript: ${stderr}`);
-            }
+            res.status(500).send(`Fehler im Skript: ${stderr}`);
+        } else {
             console.log(`Ergebnis des Python-Skripts: ${stdout}`);
             res.send(`Skript erfolgreich ausgeführt: ${stdout}`);
-        });
+        }
     });
+};
 
-app.get('/run-python/deleteAllData', (req, res) => {
-        // Relativer Pfad zum Python-Skript
-        const scriptPath = path.join(__dirname, 'scripts', 'deleteAllItems.py');
-        
-            exec(`python3 ${scriptPath}`, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`Fehler beim Ausführen des Python-Skripts: ${error.message}`);
-                    return res.status(500).send('Fehler beim Ausführen des Skripts.');
-                }
-                if (stderr) {
-                    console.error(`Fehler im Python-Skript: ${stderr}`);
-                    return res.status(500).send(`Fehler im Skript: ${stderr}`);
-                }
-                console.log(`Ergebnis des Python-Skripts: ${stdout}`);
-                res.send(`Skript erfolgreich ausgeführt: ${stdout}`);
-            });
+// API-Route für DynamoDB-Scans
+const dynamoDbScan = async (tableName, res, limit = null, lastKey = null) => {
+    const params = { TableName: tableName };
+    if (limit) params.Limit = limit;
+    if (lastKey) params.ExclusiveStartKey = JSON.parse(decodeURIComponent(lastKey));
+
+    try {
+        const command = new ScanCommand(params);
+        const data = await dynamoDbClient.send(command);
+        res.json({
+            items: data.Items,
+            lastKey: data.LastEvaluatedKey ? encodeURIComponent(JSON.stringify(data.LastEvaluatedKey)) : null,
         });
+    } catch (err) {
+        console.error(`Fehler beim Abrufen der Daten aus ${tableName}:`, err);
+        res.status(500).send(`Fehler beim Abrufen der Daten: ${err.message}`);
+    }
+};
+
+// DynamoDB-API-Routen
+app.get('/api/data/:tableName', async (req, res) => {
+    const { tableName } = req.params;
+    await dynamoDbScan(tableName, res);
+});
+
+app.get('/api/data/:tableNameLasy', async (req, res) => {
+    const { tableName } = req.params;
+    const { lastKey } = req.query;
+    await dynamoDbScan(tableName, res, 50, lastKey);
+});
+
+// Python-Skript-API-Routen
+app.get('/run-python/insertData', (req, res) => runPythonScript('importDataToDynamoDB.py', res));
+app.get('/run-python/insertDataMeasure', (req, res) => runPythonScript('importMeasureToDynamoDB.py', res));
+app.get('/run-python/deleteData', (req, res) => runPythonScript('deleteTablesAndData.py', res));
+app.get('/run-python/deleteAllData', (req, res) => runPythonScript('deleteAllItems.py', res));
 
 // Server starten
 app.listen(PORT, () => {
-    console.log(`Server läuft auf https://localhost:${PORT}`);
+    console.log(`Server läuft auf http://localhost:${PORT}`);
 });
